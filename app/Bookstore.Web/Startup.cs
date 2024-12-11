b5d6a3b2b2c86a85a589075fece416569c87608b
NoReply Amazon <no-reply@amazon.com>
2024-12-11 23:20:14 +0000

    [Commit] Iteration 21 #errors = 5 <= 6.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0103][16,13] The name 'LoggingSetup' does not exist in the current context``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,19 +1,35 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Bookstore.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             // Configure services here
+            services.AddLogging(logging =>
+            {
+                logging.AddConfiguration(Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+            });
         }
 
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            LoggingSetup.ConfigureLogging();
+            var logger = loggerFactory.CreateLogger<Startup>();
+            logger.LogInformation("Application Starting");
 
             ConfigurationSetup.ConfigureConfiguration();
 
