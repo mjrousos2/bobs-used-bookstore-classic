5b1c5134f3967b32d5e0c1cb4d3e0b6a87def57e
NoReply Amazon <no-reply@amazon.com>
2024-12-11 23:22:14 +0000

    Revert "[Commit] Iteration 23 #errors = 2 <= 2."
    This reverts commit f59de0f5da51d04dbe8b7a261985ebb78b06b813.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Bookstore.Web;
 
 namespace Bookstore.Web
 {
